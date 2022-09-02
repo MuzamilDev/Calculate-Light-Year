1e62b08a940a5304046b6da68b9c80e826eefb19
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2022-09-02 06:21:27 -0700

    Create ConvertLightYearIntoKilometers.cpp

---
+++ ConvertLightYearIntoKilometers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double Distance , LightYear;
+
+    cout<<"Enter Light Year: "<<endl;
+    cin>>LightYear;
+
+    Distance = LightYear*9000000000000;
+
+    cout<<"Total Kilometers are: "<<Distance<<endl;
+
+    return 0;
+}
